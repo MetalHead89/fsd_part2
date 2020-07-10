7e45bd21b20e90f6aa0cc3f03b19e81d9c798b6f
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-07-10 21:55:13 +0700

    refactoring dropdown code

--- modules/dropdown/dropdown.js
+++ modules/dropdown/dropdown.js
@@ -1,18 +1,47 @@
 class Dropdown {
-    constructor(dropdownHeader, quantityNumbers, buttonsSub, buttonsAdd, buttonClear, buttonApply) {
+    constructor(dropdown) {
+        isOptionVisible = false;
+        header = dropdown.getElementsByClassName('dropdown__header')[0];
+        headerText = dropdown.getElementsByClassName('dropdown__header-text')[0];
+        options = dropdown.getElementsByClassName('dropdown__options')[0];
+        quantityNumbers = dropdown.getElementsByClassName('dropdown__quantity-number');
+        buttonsSub = dropdown.getElementsByClassName('dropdown__subtract-button');
+        buttonsAdd = dropdown.getElementsByClassName('dropdown__add-button');
+        buttonClear = dropdown.getElementsByClassName('dropdown__button-clear')[0];
+        buttonApply = dropdown.getElementsByClassName('dropdown__button-apply')[0];
 
-        this.dropdownHeader = dropdownHeader;
-        this.quantityNumber1 = quantityNumbers[0];
-        this.quantityNumber2 = quantityNumbers[1];
-        this.quantityNumber3 = quantityNumbers[2];
-        this.buttonSub1 = buttonsSub[0];
-        this.buttonSub2 = buttonsSub[1];
-        this.buttonSub3 = buttonsSub[2];
-        this.buttonAdd1 = buttonsAdd[0];
-        this.buttonAdd2 = buttonsAdd[1];
-        this.buttonAdd3 = buttonsAdd[2];
-        this.buttonClear = buttonClear;
-        this.buttonApply = buttonApply;
+        quantityNumber1 = quantityNumbers[0];
+        quantityNumber2 = quantityNumbers[1];
+        quantityNumber3 = quantityNumbers[2];
+        buttonSub1 = buttonsSub[0];
+        buttonSub2 = buttonsSub[1];
+        buttonSub3 = buttonsSub[2];
+        buttonAdd1 = buttonsAdd[0];
+        buttonAdd2 = buttonsAdd[1];
+        buttonAdd3 = buttonsAdd[2];
+
+        buttonAdd1.onclick = () => this.clickAddNumberButton(buttonAdd1);
+        buttonAdd2.onclick = () => this.clickAddNumberButton(buttonAdd2);
+        buttonAdd3.onclick = () => this.clickAddNumberButton(buttonAdd3);
+        buttonSub1.onclick = () => this.clickSubNumberButton(buttonSub1);
+        buttonSub2.onclick = () => this.clickSubNumberButton(buttonSub2);
+        buttonSub3.onclick = () => this.clickSubNumberButton(buttonSub3);
+        buttonClear.onclick = () => this.clickClearButton();
+        buttonApply.onclick = () => this.generateHeader();
+        header.onclick = () => this.clickHeader();
+    }
+
+    clickHeader() {
+        if (isOptionVisible) {
+            options.style.display = "none";
+            header.removeAttribute("style")
+        }
+        else {
+            options.style.display = "flex";
+            header.style.borderRadius = "4px 4px 0 0";
+            header.style.border = "1px solid rgba(31, 32, 65, 0.5)";
+        }
+        isOptionVisible = !isOptionVisible;
     }
 
     clickAddNumberButton(button) {
@@ -24,6 +53,7 @@ class Dropdown {
             dropdownOptionNumber.previousSibling.disabled = false;
         
         this.setClearButton();
+        this.generateHeader();
     }
 
     clickSubNumberButton(button) {
@@ -35,78 +65,67 @@ class Dropdown {
             dropdownOptionNumber.previousSibling.disabled = true;
 
         this.setClearButton();
+        this.generateHeader();
     }
 
     clickClearButton() {
-        this.quantityNumber1.textContent = 0;
-        this.quantityNumber2.textContent = 0;
-        this.quantityNumber3.textContent = 0;
+        quantityNumber1.textContent = 0;
+        quantityNumber2.textContent = 0;
+        quantityNumber3.textContent = 0;
+        buttonSub1.disabled = true
+        buttonSub2.disabled = true
+        buttonSub3.disabled = true
         this.generateHeader();
         this.setClearButton();
     }
 
     generateHeader() {
-        guestsCount = parseInt(this.quantityNumber1.textContent) + parseInt(this.quantityNumber2.textContent);
-        dropdownHeader.textContent = guestsCount + ' ' + this.wordGenerator('guest', parseInt(guestsCount));
+        guestsCount = parseInt(quantityNumber1.textContent) + parseInt(quantityNumber2.textContent);
+        babiesCount = parseInt(quantityNumber3.textContent);
+
+        if (guestsCount == 0 && babiesCount == 0)
+            headerText.textContent = 'Сколько гостей';
+        else if (guestsCount > 0 && babiesCount == 0)
+            headerText.textContent = guestsCount + ' ' + this.wordGenerator('guest', parseInt(guestsCount));
+        else if (guestsCount > 0 && babiesCount > 0)
+            headerText.textContent = `${guestsCount} ${this.wordGenerator('guest', parseInt(guestsCount))}, 
+                ${babiesCount} ${this.wordGenerator('baby', parseInt(babiesCount))}`;
     }
 
     setClearButton() {
-        if (parseInt(this.quantityNumber1.textContent) + parseInt(this.quantityNumber2.textContent) + parseInt(this.quantityNumber3.textContent) > 0) {
-            this.buttonClear.style.display = 'inline-block';
+        if (parseInt(quantityNumber1.textContent) + parseInt(quantityNumber2.textContent) + parseInt(quantityNumber3.textContent) > 0) {
+            buttonClear.style.display = 'inline-block';
         }
         else
-            this.buttonClear.style.display = 'none';
+            buttonClear.style.display = 'none';
     }
 
     wordGenerator(word, number) {
         number10 = number % 10;
         number100 = number % 100;
-        if (number10 == 1 && number100 != 11)
-            return 'гость';
-        else if ((number10 >= 2 && number10 <= 4) && !(number100 >= 12 && number100 <= 14))
-            return 'гостя';
-        else
-            return 'гостей';
-            
+        if (number10 == 1 && number100 != 11){
+            if (word == 'guest')
+                return 'гость';
+            else if (word == 'baby')
+                return 'младенец'
+        }
+        else if ((number10 >= 2 && number10 <= 4) && !(number100 >= 12 && number100 <= 14)) {
+            if (word == 'guest')
+                return 'гостя';
+            else if (word == 'baby')
+                return 'младенца'
+        }
+        else {
+            if (word == 'guest')
+                return 'гостей';
+            else if (word == 'baby')
+                return 'младенцев'
+        }
     }
 }
 
-var dropdownElements = document.getElementsByClassName('dropdown__header');
-var optionsVisible = false
-
-for (i = 0; i < dropdownElements.length; i++) {
-    options = dropdownElements[i].nextSibling;
-    dropdown = dropdownElements[i]
-    dropdown.onclick = function() {
-        if (optionsVisible) {
-            options.style.display = "none";
-            dropdown.removeAttribute("style")
-        }
-        else {
-            options.style.display = "flex";
-            dropdown.style.borderRadius = "4px 4px 0 0"
-            dropdown.style.border = "1px solid rgba(31, 32, 65, 0.5)";
-        }
-        optionsVisible = !optionsVisible
-    };
-};
-
-var dropdowns = document.getElementsByClassName('dropdown_guests');
-for (i = 0; i < dropdowns.length; i++) {
-    var dropdownHeader = dropdowns[i].getElementsByClassName('dropdown__header-text')[0];
-    var quantityNumbers = dropdowns[i].getElementsByClassName('dropdown__quantity-number');
-    var buttonsSub = dropdowns[i].getElementsByClassName('dropdown__subtract-button');
-    var buttonsAdd = dropdowns[i].getElementsByClassName('dropdown__add-button');
-    var buttonClear = dropdowns[i].getElementsByClassName('dropdown__button-clear')[0];
-    var buttonApply = dropdowns[i].getElementsByClassName('dropdown__button-apply')[0];
-
-    let dropdown = new Dropdown(dropdownHeader, quantityNumbers, buttonsSub, buttonsAdd, buttonClear, buttonApply);
-    dropdown.buttonAdd1.onclick = function() {dropdown.clickAddNumberButton(dropdown.buttonAdd1)};
-    dropdown.buttonAdd2.onclick = function() {dropdown.clickAddNumberButton(dropdown.buttonAdd2)};
-    dropdown.buttonAdd3.onclick = function() {dropdown.clickAddNumberButton(dropdown.buttonAdd3)};
-    dropdown.buttonSub1.onclick = function() {dropdown.clickSubNumberButton(dropdown.buttonSub1)};
-    dropdown.buttonSub2.onclick = function() {dropdown.clickSubNumberButton(dropdown.buttonSub2)};
-    dropdown.buttonSub3.onclick = function() {dropdown.clickSubNumberButton(dropdown.buttonSub3)};
-    dropdown.buttonClear.onclick = function() {dropdown.clickClearButton()};
-    dropdown.buttonApply.onclick = function() {dropdown.generateHeader()};
-}
+let dr = [];
+var dropdowns = document.getElementsByClassName('dropdown');
+for (dropdownElement of dropdowns) {
+    dr.push(new Dropdown(dropdownElement));
+} 
