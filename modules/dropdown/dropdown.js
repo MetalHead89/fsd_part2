4310601190d8735bdde646b0a5c33f49f401d024
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-07-10 22:52:56 +0700

    bug opening dropdowns fixed

--- modules/dropdown/dropdown.js
+++ modules/dropdown/dropdown.js
@@ -1,108 +1,116 @@
 class Dropdown {
     constructor(dropdown) {
-        isOptionVisible = false;
-        header = dropdown.getElementsByClassName('dropdown__header')[0];
-        headerText = dropdown.getElementsByClassName('dropdown__header-text')[0];
-        options = dropdown.getElementsByClassName('dropdown__options')[0];
-        quantityNumbers = dropdown.getElementsByClassName('dropdown__quantity-number');
-        buttonsSub = dropdown.getElementsByClassName('dropdown__subtract-button');
-        buttonsAdd = dropdown.getElementsByClassName('dropdown__add-button');
-        buttonClear = dropdown.getElementsByClassName('dropdown__button-clear')[0];
-        buttonApply = dropdown.getElementsByClassName('dropdown__button-apply')[0];
+        let quantityNumbers = dropdown.getElementsByClassName('dropdown__quantity-number');
+        let buttonsSub = dropdown.getElementsByClassName('dropdown__subtract-button');
+        let buttonsAdd = dropdown.getElementsByClassName('dropdown__add-button');
 
-        quantityNumber1 = quantityNumbers[0];
-        quantityNumber2 = quantityNumbers[1];
-        quantityNumber3 = quantityNumbers[2];
-        buttonSub1 = buttonsSub[0];
-        buttonSub2 = buttonsSub[1];
-        buttonSub3 = buttonsSub[2];
-        buttonAdd1 = buttonsAdd[0];
-        buttonAdd2 = buttonsAdd[1];
-        buttonAdd3 = buttonsAdd[2];
+        this.isOptionVisible = false;
+        this.header = dropdown.getElementsByClassName('dropdown__header')[0];
+        this.headerText = dropdown.getElementsByClassName('dropdown__header-text')[0];
+        this.options = dropdown.getElementsByClassName('dropdown__options')[0];
+        this.quantityNumber1 = quantityNumbers[0];
+        this.quantityNumber2 = quantityNumbers[1];
+        this.quantityNumber3 = quantityNumbers[2];
+        this.buttonSub1 = buttonsSub[0];
+        this.buttonSub2 = buttonsSub[1];
+        this.buttonSub3 = buttonsSub[2];
+        this.buttonAdd1 = buttonsAdd[0];
+        this.buttonAdd2 = buttonsAdd[1];
+        this.buttonAdd3 = buttonsAdd[2];
 
-        buttonAdd1.onclick = () => this.clickAddNumberButton(buttonAdd1);
-        buttonAdd2.onclick = () => this.clickAddNumberButton(buttonAdd2);
-        buttonAdd3.onclick = () => this.clickAddNumberButton(buttonAdd3);
-        buttonSub1.onclick = () => this.clickSubNumberButton(buttonSub1);
-        buttonSub2.onclick = () => this.clickSubNumberButton(buttonSub2);
-        buttonSub3.onclick = () => this.clickSubNumberButton(buttonSub3);
-        buttonClear.onclick = () => this.clickClearButton();
-        buttonApply.onclick = () => this.generateHeader();
-        header.onclick = () => this.clickHeader();
+        this.buttonAdd1.onclick = () => this.clickAddNumberButton(this.buttonAdd1);
+        this.buttonAdd2.onclick = () => this.clickAddNumberButton(this.buttonAdd2);
+        this.buttonAdd3.onclick = () => this.clickAddNumberButton(this.buttonAdd3);
+        this.buttonSub1.onclick = () => this.clickSubNumberButton(this.buttonSub1);
+        this.buttonSub2.onclick = () => this.clickSubNumberButton(this.buttonSub2);
+        this.buttonSub3.onclick = () => this.clickSubNumberButton(this.buttonSub3);
+        this.header.onclick = () => this.clickHeader();
+
+        if (dropdown.className.includes('dropdown_guests')) {
+            this.buttonClear = dropdown.getElementsByClassName('dropdown__button-clear')[0];
+            this.buttonApply = dropdown.getElementsByClassName('dropdown__button-apply')[0];
+            this.buttonClear.onclick = () => this.clickClearButton();
+            this.buttonApply.onclick = () => this.generateHeader();
+            this.dropdownType = 'guests';
+        }
+        else
+            this.dropdownType = 'comfort';
     }
 
     clickHeader() {
-        if (isOptionVisible) {
-            options.style.display = "none";
-            header.removeAttribute("style")
+        if (this.isOptionVisible) {
+            this.options.style.display = "none";
+            this.header.removeAttribute("style")
         }
         else {
-            options.style.display = "flex";
-            header.style.borderRadius = "4px 4px 0 0";
-            header.style.border = "1px solid rgba(31, 32, 65, 0.5)";
+            this.options.style.display = "flex";
+            this.header.style.borderRadius = "4px 4px 0 0";
+            this.header.style.border = "1px solid rgba(31, 32, 65, 0.5)";
         }
-        isOptionVisible = !isOptionVisible;
+        this.isOptionVisible = !this.isOptionVisible;
     }
 
     clickAddNumberButton(button) {
         dropdownOptionNumber = button.previousSibling;
         number = parseInt(dropdownOptionNumber.textContent);
-        dropdownOptionNumber.textContent = number + 1
+        dropdownOptionNumber.textContent = number + 1;
 
         if (number == 0) 
             dropdownOptionNumber.previousSibling.disabled = false;
         
-        this.setClearButton();
+        if (this.dropdownType == 'guests')
+            this.setClearButton();
         this.generateHeader();
     }
 
     clickSubNumberButton(button) {
         dropdownOptionNumber = button.nextSibling;
         number = parseInt(dropdownOptionNumber.textContent);
-        dropdownOptionNumber.textContent = number - 1
+        dropdownOptionNumber.textContent = number - 1;
     
         if (number == 1)
             dropdownOptionNumber.previousSibling.disabled = true;
 
-        this.setClearButton();
+        if (this.dropdownType == 'guests')
+            this.setClearButton();
         this.generateHeader();
     }
 
     clickClearButton() {
-        quantityNumber1.textContent = 0;
-        quantityNumber2.textContent = 0;
-        quantityNumber3.textContent = 0;
-        buttonSub1.disabled = true
-        buttonSub2.disabled = true
-        buttonSub3.disabled = true
+        this.quantityNumber1.textContent = 0;
+        this.quantityNumber2.textContent = 0;
+        this.quantityNumber3.textContent = 0;
+        this.buttonSub1.disabled = true;
+        this.buttonSub2.disabled = true;
+        this.buttonSub3.disabled = true;
         this.generateHeader();
         this.setClearButton();
     }
 
     generateHeader() {
-        guestsCount = parseInt(quantityNumber1.textContent) + parseInt(quantityNumber2.textContent);
-        babiesCount = parseInt(quantityNumber3.textContent);
+        let guestsCount = parseInt(this.quantityNumber1.textContent) + parseInt(this.quantityNumber2.textContent);
+        let babiesCount = parseInt(this.quantityNumber3.textContent);
 
         if (guestsCount == 0 && babiesCount == 0)
-            headerText.textContent = 'Сколько гостей';
+            this.headerText.textContent = 'Сколько гостей';
         else if (guestsCount > 0 && babiesCount == 0)
-            headerText.textContent = guestsCount + ' ' + this.wordGenerator('guest', parseInt(guestsCount));
+            this.headerText.textContent = guestsCount + ' ' + this.wordGenerator('guest', parseInt(guestsCount));
         else if (guestsCount > 0 && babiesCount > 0)
-            headerText.textContent = `${guestsCount} ${this.wordGenerator('guest', parseInt(guestsCount))}, 
+            this.headerText.textContent = `${guestsCount} ${this.wordGenerator('guest', parseInt(guestsCount))}, 
                 ${babiesCount} ${this.wordGenerator('baby', parseInt(babiesCount))}`;
     }
 
     setClearButton() {
-        if (parseInt(quantityNumber1.textContent) + parseInt(quantityNumber2.textContent) + parseInt(quantityNumber3.textContent) > 0) {
-            buttonClear.style.display = 'inline-block';
+        if (parseInt(this.quantityNumber1.textContent) + parseInt(this.quantityNumber2.textContent) + parseInt(this.quantityNumber3.textContent) > 0) {
+            this.buttonClear.style.display = 'inline-block';
         }
         else
-            buttonClear.style.display = 'none';
+            this.buttonClear.style.display = 'none';
     }
 
     wordGenerator(word, number) {
-        number10 = number % 10;
-        number100 = number % 100;
+        let number10 = number % 10;
+        let number100 = number % 100;
         if (number10 == 1 && number100 != 11){
             if (word == 'guest')
                 return 'гость';
