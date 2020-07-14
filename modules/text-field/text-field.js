499029ce4cb0e8d3c2d20590176e8207f9d295d2
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-07-14 22:00:12 +0700

    masked text field bugs fixed

--- modules/text-field/text-field.js
+++ modules/text-field/text-field.js
@@ -1,4 +1,4 @@
-function getCaretPosition(ctrl) {
+/*function getCaretPosition(ctrl) {
     if (ctrl.selectionStart || ctrl.selectionStart == '0')
         return ctrl.selectionStart;
 }
@@ -7,12 +7,69 @@ function cancelInput(endPosition, text, inputText){
     return `${text.slice(0, endPosition - inputText.length)}${text.slice(endPosition)}`
 }
 
+function onKeyPressMaskedTextField(maskedTextField) {
+    maskedPrevText = maskedTextField.value;
+}*/
+
+
+/*function deleteMaskedText(text) {
+    text = text.split('.').join('');
+    if (text.length >= 2 && text.length < 4)
+        text = `${text.slice(0, 2)}.${text.slice(2)}`;
+    else if (text.length >= 4)        
+        text = `${text.slice(0, 2)}.${text.slice(2, 4)}.${text.slice(4, 8)}`
+    return text;
+}*/
 
 
 
-function changedMaskedTextField(event, maskedTextField) {
-    console.dir(event)
-    alert(maskedTextField.value)
+function onInputMaskedTextField(event, maskedTextField) {
+    let caretPosition = maskedTextField.selectionStart;
+    let text = maskedTextField.value.split('.').join('');
+
+    if (text.length >= 2 && text.length < 4) {
+        text = `${text.slice(0, 2)}.${text.slice(2)}`;
+        caretPosition += 1;
+    }        
+    else if (text.length >= 4) {
+        text = `${text.slice(0, 2)}.${text.slice(2, 4)}.${text.slice(4, 8)}`
+        caretPosition += 2;
+    }
+        
+    maskedTextField.value = text;
+    maskedTextField.selectionStart = caretPosition;
+    maskedTextField.selectionEnd = caretPosition;
+    /*maskedTextField.value = deleteMaskedText(maskedTextField.value);
+    if(event.inputType == 'deleteContentBackward' || event.inputType == 'deleteContentForward' || 
+        event.inputType == 'deleteByCut' || event.inputType == 'insertFromPaste')
+        maskedTextField.value = deleteMaskedText(maskedTextField.value);
+    else {
+        if (isNaN(parseInt(event.data)))
+            maskedTextField.value = maskedTextField.value.slice(0, -1);
+    
+        if (maskedTextField.value.length == 1 && parseInt(maskedTextField.value) > 3)
+            maskedTextField.value = '0' + maskedTextField.value;
+        else if (maskedTextField.value.length == 4 && parseInt(event.data) > 1)
+        {
+            maskedTextField.value = `${maskedTextField.value.slice(0, -1)}0${event.data}`;
+        }
+            
+
+        if (maskedTextField.value.length == 2 || maskedTextField.value.length == 5)
+            maskedTextField.value += '.';
+        else if (maskedTextField.value.length == 11)
+            maskedTextField.value = maskedTextField.value.slice(0, -1);
+    }*/
+
+
+
+
+
+
+
+
+
+    /*event.preventDefault();*/
     /*if (isNaN(parseInt(event.data)))
         maskedTextField.value = maskedTextField.value.slice(0, -1);
     
@@ -33,7 +90,7 @@ function changedMaskedTextField(event, maskedTextField) {
 
 
 
-    let regexp = '';
+    /*let regexp = '';
 
     switch(maskedTextField.value.length){
         case 1:
@@ -66,7 +123,7 @@ function changedMaskedTextField(event, maskedTextField) {
         maskedTextField.value = cancelInput(maskedTextField.selectionStart, maskedTextField.value, event.data);
     else
         if (maskedTextField.value.length == 2 || maskedTextField.value.length == 5)
-            maskedTextField.value += '.';
+            maskedTextField.value += '.';*/
 
 
 
@@ -77,5 +134,11 @@ function changedMaskedTextField(event, maskedTextField) {
 
 
 let maskedTextField = document.querySelector('.text-field_masked');
-if (maskedTextField)
-    maskedTextField.addEventListener('input', () => changedMaskedTextField(event, maskedTextField));
+/*let maskedPrevText = ''*/
+if (maskedTextField) {
+    maskedTextField.addEventListener('input', () => onInputMaskedTextField(event, maskedTextField));
+    /*maskedTextField.addEventListener('keypress', () => onKeyPressMaskedTextField(maskedTextField));
+    maskedTextField.addEventListener('paste', () => event.preventDefault());
+    maskedTextField.addEventListener('cut', () => event.preventDefault());
+    maskedTextField.addEventListener('keydown', () => changedMaskedTextField(event, maskedTextField));*/
+}
