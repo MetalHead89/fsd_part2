dc7d8215b4d5e313688c29061413658dc56a2c84
bakalov <bakalov.vitaliy@gmail.com>
2020-07-25 22:40:11 +0700

    modified range-slider

--- modules/range-slider/range-slider.js
+++ modules/range-slider/range-slider.js
@@ -1,25 +1,35 @@
-let minValueSlider = document.querySelector('.range-slider__min-value-slider');
-minValueSlider.onmousedown = onMouseDownMinValueSlider;
+let leftInput = document.querySelector('.range-slider__left-input');
+let rightInput = document.querySelector('.range-slider__right-input');
 
-function onMouseDownMinValueSlider() {
-    if (this.style.left === '')
-        this.style.left = '0px';
-    
-    let xOffset = event.pageX - parseInt(this.style.left);
+let leftThumb = document.querySelector('.range-slider__left-thumb');
+let rightThumb = document.querySelector('.range-slider__right-thumb');
+let range = document.querySelector('.range-slider__range');
 
-    document.onmouseup = function() {
-        document.onmousemove = null;
-        this.onmouseup = null;
-    }
+function setLeftValue() {
+    let _this = leftInput;
+    let min = parseInt(_this.min);
+    let max = parseInt(_this.max);
 
-    document.onmousemove = () => moveSlider(this, xOffset);
+    _this.value = Math.min(parseInt(_this.value), parseInt(rightInput.value) - 1);
+    let percent = ((_this.value - min) / (max - min)) * 100;
+    leftThumb.style.left = percent + '%';
+    range.style.left = percent + '%';
 }
 
-function moveSlider(slider, offset) {
-    let shift = event.pageX - offset;
-    let parent = slider.parentElement;
-    console.dir(slider)
+setLeftValue();
 
-    if (shift >= 0 && shift <= parent.offsetWidth - slider.offsetWidth - slider.clientLeft)
-        slider.style.left = shift + 'px';
+function setRightValue() {
+    let _this = rightInput;
+    let min = parseInt(_this.min);
+    let max = parseInt(_this.max);
+
+    _this.value = Math.max(parseInt(_this.value), parseInt(leftInput.value) + 1);
+    let percent = ((_this.value - min) / (max - min)) * 100;
+    rightThumb.style.right = 100 - percent + '%';
+    range.style.right = 100 - percent + '%';
 }
+
+setRightValue();
+
+leftInput.addEventListener('input', setLeftValue);
+rightInput.addEventListener('input', setRightValue);
