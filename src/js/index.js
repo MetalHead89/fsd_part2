078ce5cbd0054fea0a1193e88b53f41efe8b7909
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-07-31 22:57:16 +0700

    style variables created

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
 import '@/scss/style.scss';
 import '@/pug/modules/color-block/color-block.scss';
 import '@/scss/fonts.scss';
+import '@/scss/UIkit.scss';
+import '@/pug/modules/color-block/color-block.scss'
+import '@/pug/modules/type-block/type-block.scss'
