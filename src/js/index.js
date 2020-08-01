f074d6f3ee9f38e1ae43cf5a74f4fec2771629a4
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-08-01 12:15:43 +0700

    refactoring of the color-block module

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import '@/scss/style.scss';
 import '@/pug/modules/color-block/color-block.scss';
 import '@/scss/fonts.scss';
-import '@/scss/UIkit.scss';
+import '@/scss/ui_kit.scss';
 import '@/pug/modules/color-block/color-block.scss'
 import '@/pug/modules/type-block/type-block.scss'
