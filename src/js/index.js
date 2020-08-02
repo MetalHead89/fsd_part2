5610ebb5ab6982fc65bc3e54e0475c2270fa26d8
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-08-02 18:29:12 +0700

    refactoring of the button module

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 //Scripts
 import '@/pug/modules/text-field/text-field.js'
+import '@/pug/modules/dropdown/dropdown.js'
 
 // Styles
 import '@/scss/style.scss';
@@ -9,3 +10,6 @@ import '@/scss/ui_kit.scss';
 import '@/pug/modules/color-block/color-block.scss'
 import '@/pug/modules/type-block/type-block.scss'
 import '@/pug/modules/text-field/text-field.scss'
+import '@/pug/modules/dropdown/dropdown.scss'
+
+import '@/pug/modules/button/button.scss'
