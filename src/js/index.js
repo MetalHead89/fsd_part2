4a0a2bd76a5bb552ac6ef9aa549ee15248ac6b2c
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-08-02 12:24:10 +0700

    refactoring the text-field module

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,11 @@
+//Scripts
+import '@/pug/modules/text-field/text-field.js'
+
+// Styles
 import '@/scss/style.scss';
 import '@/pug/modules/color-block/color-block.scss';
 import '@/scss/fonts.scss';
 import '@/scss/ui_kit.scss';
 import '@/pug/modules/color-block/color-block.scss'
 import '@/pug/modules/type-block/type-block.scss'
+import '@/pug/modules/text-field/text-field.scss'
--- src/pug/modules/text-field/text-field.js
+++ src/pug/modules/text-field/text-field.js
@@ -1,13 +1,27 @@
+let maskedTextField = document.querySelector('.text-field__field-masked');
+
+if (maskedTextField) {
+    maskedTextField.oninput = onInputMaskedTextField;
+    maskedTextField.onpaste = onPastetMaskedTextField;
+    maskedTextField.onkeydown = onKeydowMaskedTextField;
+}
+
 function getTextWithoutDots(text) {
-    // Убирает все точки в тексте
-    // 16.07.2020 => 16072020
+    /**
+     * Удаляет точки из полученной строки
+     * 
+     * @param {string} text - строка с точками
+     * @returns {string} Строка без точек
+     */
 
     return text.split('.').join('');
 }
 
 function onPastetMaskedTextField() {
-    // Проверяет, что вставляемый текст содержит в себе только цифры и точки.
-    // В случае неудачной проверки отменяет вставку
+    /**
+     * Проверяет, что вставляемый текст содержит в себе только цифры и точки.
+     * В случае неудачной проверки отменяет вставку
+     */
 
     clipboardData = event.clipboardData || window.clipboardData;
     pastedData = clipboardData.getData('Text');
@@ -16,24 +30,30 @@ function onPastetMaskedTextField() {
         event.preventDefault();
 }
 
-function onKeydowMaskedTextField(event, maskedTextField) {
-    // Сдвигает каретку во время удаления, если она стоит до или перед точкой
+function onKeydowMaskedTextField() {
+    /**
+     * Сдвигает каретку во время удаления, если она стоит до или перед точкой
+     */
     
-    if (event.key == 'Backspace' && maskedTextField.selectionStart == maskedTextField.selectionEnd && 
-    maskedTextField.value.substr(maskedTextField.selectionStart - 1, 1) == '.') {
-        maskedTextField.selectionStart--;
-        maskedTextField.selectionEnd--;
+    if (event.key == 'Backspace' && this.selectionStart == this.selectionEnd && 
+    this.value.substr(this.selectionStart - 1, 1) == '.') {
+        this.selectionStart--;
+        this.selectionEnd--;
     }
-    if (event.key == 'Delete' && maskedTextField.selectionStart == maskedTextField.selectionEnd && 
-    maskedTextField.value.substr(maskedTextField.selectionStart, 1) == '.') {
-        maskedTextField.selectionStart++;
-        maskedTextField.selectionEnd++;
+    if (event.key == 'Delete' && this.selectionStart == this.selectionEnd && 
+    this.value.substr(this.selectionStart, 1) == '.') {
+        this.selectionStart++;
+        this.selectionEnd++;
     }
 }
 
-function onInputMaskedTextField(event, maskedTextField) {
-    let caretPosition = maskedTextField.selectionStart;
-    let text = maskedTextField.value;
+function onInputMaskedTextField() {
+    /**
+     * Ограничивает ввод символов, не являющихся буквами
+     * Расставляет разделители.
+     */
+    let caretPosition = this.selectionStart;
+    let text = this.value;
     
     if (isNaN(getTextWithoutDots(text))) {
         startCaretPosition = caretPosition - event.data.length;
@@ -44,25 +64,11 @@ function onInputMaskedTextField(event, maskedTextField) {
             caretPosition--;
     }
 
-    if (event.inputType == 'deleteContentBackward' && text.length > 2) {
-        caretPosition--;
-    }
-
-    if (event.inputType == 'deleteContentForward' && text.length > 2) {
+    if ((event.inputType == 'deleteContentBackward' || event.inputType == 'deleteContentForward') && text.length > 2) {
         caretPosition--;
     }
 
     text = getTextWithoutDots(text);
-
-    /*if (text[0] && parseInt(text[0]) > 3) {
-        text = `0${text}`;
-        caretPosition++; 
-    }
-
-    if (text[2] && parseInt(text[2]) > 1) {
-        text = `${text.slice(0, 2)}0${text.slice(2)}`
-        caretPosition ++;
-    }*/
     
     if (text.length >= 2 && text.length < 4) {
         text = `${text.slice(0, 2)}.${text.slice(2)}`;
@@ -73,14 +79,7 @@ function onInputMaskedTextField(event, maskedTextField) {
         caretPosition ++;
     }
         
-    maskedTextField.value = text;
-    maskedTextField.selectionStart = caretPosition;
-    maskedTextField.selectionEnd = caretPosition;
-}
-
-let maskedTextField = document.querySelector('.text-field_masked');
-if (maskedTextField) {
-    maskedTextField.addEventListener('input', () => onInputMaskedTextField(event, maskedTextField));
-    maskedTextField.addEventListener('paste', () => onPastetMaskedTextField(event, maskedTextField));
-    maskedTextField.addEventListener('keydown', () => onKeydowMaskedTextField(event, maskedTextField));
+    this.value = text;
+    this.selectionStart = caretPosition;
+    this.selectionEnd = caretPosition;
 }
