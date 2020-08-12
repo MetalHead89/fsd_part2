5f27b9e0a4616f263485d37bb85fe1bf4c7a669b
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-08-12 20:27:53 +0700

    added calendar class to calendar module

--- src/pug/modules/calendar/calendar.js
+++ src/pug/modules/calendar/calendar.js
@@ -1,78 +1,124 @@
 'use strict'
 
-const MONTHS = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-const currentDate = new Date();
+class Calendar {
+    constructor(calendar) {
+        this.MONTHS = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+
+        this.currentDate = new Date();
+        this.calendar = calendar;
+        this.calendarTitle = calendar.querySelector('.calendar__month');
+        this.startDate = null;
+        this.endDate = null;
+        this.year = this.currentDate.getFullYear();
+        this.month = this.currentDate.getMonth();
+
+        this.prevMonthButton = calendar.querySelector('.calendar__prev-month');
+        this.nextMonthButton = calendar.querySelector('.calendar__next-month');
+
+        this.prevMonthButton.onclick = () => this.switchMonth(this.prevMonthButton);
+        this.nextMonthButton.onclick = () => this.switchMonth(this.nextMonthButton);
+    }
+
+    switchMonth(button) {
+        const date = button.classList.contains('calendar__prev-month') ? new Date(this.year, this.month - 1) : new Date(this.year, this.month + 1)
+
+        // if (button.classList.contains('prevMonthButton')) {
+        //     this.month--;
+        // } else {
+        //     this.month++;
+        // }
+
+        // this.refreshCalendar(new Date(this.year, this.month));
+        this.refreshCalendar(date);
+    }
+
+    // addMonth() {
+    //     return new Date(this.year, this.month + 1)
+    // }
+
+    // subtractMonth() {
+    //     return new Date(this.year, this.month - 1)
+    //     // this.month - 1 < 0 ? new Date(this.year - 1, 11) : new Date(this.year, this.month - 1)
+    // }
+
+    refreshCalendar(date=new Date(this.year, this.month)) {
+        /**
+         * Обновляет информацию на календаре
+         */
+    
+        this.removeCalendar();
+        this.createNewCalendar(date);
+    }
+
+    removeCalendar() {
+        /**
+         * Удаляет текущий календарь
+         */
+
+        const weekBloks = this.calendar.querySelectorAll('.calendar__week');
+        
+        for (let weekNode of weekBloks) {
+            weekNode.remove();
+        }
+    }
+
+    createNewCalendar(date) {
+        /**
+         * Создает новый календарь
+         */
+        this.year = date.getFullYear();
+        this.month = date.getMonth();
+
+        const daysInCurrentMonth = 32 - new Date(this.year, this.month, 32).getDate();
+        const daysInPrevMonths = 32 - new Date(this.year, this.month - 1, 32).getDate();
+        const firstDayMonth = date.getDay() == 0 ? 7 : date.getDate();
+        console.log(date)
+        const calendarBody = this.calendar.querySelector('.calendar__days');
+        const weeksInCalendar = Math.ceil((daysInCurrentMonth + firstDayMonth - 1) / 7);
+        
+        this.calendarTitle.innerText = `${this.MONTHS[this.month]} ${this.year}`;
+
+        let dayNumber = 1;
+        let dayInMonth = daysInCurrentMonth;
+        if (firstDayMonth != 0) {
+            dayNumber = daysInPrevMonths - firstDayMonth - 1;
+            dayInMonth = daysInPrevMonths;
+        }
+        
+        for (let week = 0; week <= weeksInCalendar; week++) {
+            const weekDiv = document.createElement('div');
+            weekDiv.className = 'calendar__week';
+
+            for (let weekDayNumber = 0; weekDayNumber < 7; weekDayNumber++) {
+                if (dayNumber > dayInMonth) {
+                    dayNumber = 1;
+                    dayInMonth = daysInCurrentMonth;
+                }
+                const calendarDay = document.createElement('span');
+                calendarDay.classList = 'calendar__day calendar__day-number_current-month';
+                calendarDay.innerText = dayNumber;
+                weekDiv.append(calendarDay);
+
+                dayNumber++;
+            }
+
+            calendarBody.append(weekDiv);
+        }
+
+        // let day = daysInCurrentMonth;
+        // if (weekDay != 0) {
+        //     day = 
+        // }
+        // for (let day = daysInPrevMonths - )
+
+        // const week = document.createElement('div');
+        // week.className = 'calendar__week';
+    }
+}
+
 const calendars = document.querySelectorAll('.calendar');
 
-for (let calendar of calendars) {
-    refreshCalendar.bind(calendar, currentDate)();
-
-    const prevMonthButton = calendar.querySelector('.calendar__prev-month');
-    const nextMonthButton = calendar.querySelector('.calendar__next-month');
-
-    prevMonthButton.onclick = () => switchMonth.bind(prevMonthButton, calendar)();
-    nextMonthButton.onclick = () => switchMonth.bind(nextMonthButton, calendar)();
+for (let calendarInThePage of calendars) {
+    const calendar = new Calendar(calendarInThePage);
+    calendar.refreshCalendar();
 }
-
-function refreshCalendar(date) {
-    /**
-     * Обновляет информацию на календаре
-     */
-
-    removeCalendar.bind(this)();
-    createNewCalendar.bind(this)(date);
-}
-
-function removeCalendar() {
-    /**
-     * Удаляет текущий календарь
-     */
-
-    const weekBloks = this.querySelectorAll('.calendar__week');
-    
-    for (let weekNode of weekBloks) {
-        weekNode.remove();
-    }
-}
-
-function createNewCalendar(date) {
-    /**
-     * Создает новый календарь
-     */
-
-     console.dir(date)
-    let calendar = {};
-    let calendarTitle = this.querySelector('.calendar__month');
-
-    calendar.year = date.getFullYear();
-    calendar.month = date.getMonth();
-    calendar.daysInMonth = 32 - new Date(calendar.year, calendar.month, 32).getDate();
-    calendar.weekDay = date.getDay();
-    
-    calendarTitle.innerText = `${MONTHS[calendar.month]} ${calendar.year}`;
-}
-
-function switchMonth(calendar) {
-    const calendarTitle = calendar.querySelector('.calendar__month').innerText.split(' ');
-    let month = MONTHS.indexOf(calendarTitle[0]);
-    const year = calendarTitle[1];
-
-    if (this.classList.contains('calendar__prev-month')) {
-        month--;
-    } else {
-        month++;
-    }
-    
-    refreshCalendar.bind(calendar, new Date(year, month));
-}
-
-//-     - const WEEK_DAYS = 7
-//-     - const currentDate = new Date()    
-//-     - const currentMonth = currentDate.getMonth
-
-//-     - let weeksCount = 5
-//-     - let week = 1    
-//-     - let dayNumber = 1
-//-     - let calendarDayNumberClass = 'calendar__day calendar__day-number_current-month'
-//-     - let headerText = 'Сентябрь 1989'
-//-     - let currentDay = 6
