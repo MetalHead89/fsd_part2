e4c030cd6b88e154d4eb5eebfed671d2fe887f68
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-08-16 21:58:58 +0700

    added style for slide change buttons

---
+++ src/pug/modules/numberCard/numberCard.js
@@ -0,0 +1,13 @@
+class NumberCard {
+    constructor(numberCard) {
+        this.numberCard = numberCard;
+        // this.slider = numberCard.querySelector('.number-card__slider-section');
+    }
+}
+
+
+const numbersCards = document.querySelectorAll('.number-card');
+
+// for (card in numbersCards) {
+
+// }
