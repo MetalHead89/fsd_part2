e376e983949dc1a850784e1027d33fd2c560162b
Bakalov Vitalij <bakalov.vitaliy@gmail.com>
2020-08-17 05:56:21 +0700

    added slide switching to number-card module

--- src/pug/modules/numberCard/numberCard.js
+++ src/pug/modules/numberCard/numberCard.js
@@ -1,13 +1,41 @@
+'use strict'
+
 class NumberCard {
     constructor(numberCard) {
         this.numberCard = numberCard;
-        // this.slider = numberCard.querySelector('.number-card__slider-section');
+
+        this.buttonPrev = numberCard.querySelector('.number-card__prevButton');
+        this.buttonNext = numberCard.querySelector('.number-card__nextButton');
+
+        this.buttonNext.onclick = () =>  this.switchToNextSlide();
+        this.buttonPrev.onclick = () =>  this.switchToPreviousSlide();
+    }
+
+    switchToNextSlide() {
+        const activeSlide = this.numberCard.querySelector('.number-card__slider-list-item_opacity1');
+        const nextSlide = activeSlide.nextElementSibling;
+
+        activeSlide.classList.remove('number-card__slider-list-item_opacity1');
+        activeSlide.classList.add('number-card__slider-list-item_opacity0');
+        nextSlide.classList.remove('number-card__slider-list-item_opacity0');
+        nextSlide.classList.add('number-card__slider-list-item_opacity1');
+    }
+
+    switchToPreviousSlide(){
+        const activeSlide = this.numberCard.querySelector('.number-card__slider-list-item_opacity1');
+        const prevSlide = activeSlide.previousElementSibling;
+
+        activeSlide.classList.remove('number-card__slider-list-item_opacity1');
+        activeSlide.classList.add('number-card__slider-list-item_opacity0');
+        prevSlide.classList.remove('number-card__slider-list-item_opacity0');
+        prevSlide.classList.add('number-card__slider-list-item_opacity1');
     }
 }
 
 
 const numbersCards = document.querySelectorAll('.number-card');
 
-// for (card in numbersCards) {
+for (let card of numbersCards) {
+    let numberCard = new NumberCard(card);
 
-// }
+}
