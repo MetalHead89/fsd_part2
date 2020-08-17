a5e7a8a011732476f6070db9fcf59bcb8c8d77b4
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2020-08-17 08:48:09 +0700

    added point change when changing slide in the number-card module

--- src/pug/modules/numberCard/numberCard.js
+++ src/pug/modules/numberCard/numberCard.js
@@ -14,21 +14,39 @@ class NumberCard {
     switchToNextSlide() {
         const activeSlide = this.numberCard.querySelector('.number-card__slider-list-item_opacity1');
         const nextSlide = activeSlide.nextElementSibling;
+        const activeDot = this.numberCard.querySelector('.number-card__active-dot');
+        const nextDot = activeDot.nextElementSibling;
 
+        if (nextSlide) {
+            this.switchSlide(activeSlide, nextSlide);
+            this.switchSlideDot(activeDot, nextDot);
+        }
+    }
+
+    switchToPreviousSlide(){
+        const activeSlide = this.numberCard.querySelector('.number-card__slider-list-item_opacity1');
+        const prevSlide = activeSlide.previousElementSibling;
+        const activeDot = this.numberCard.querySelector('.number-card__active-dot');
+        const prevDot = activeDot.previousElementSibling;
+
+        if (prevSlide) {
+            this.switchSlide(activeSlide, prevSlide);
+            this.switchSlideDot(activeDot, prevDot);
+        }
+    }
+
+    switchSlide(activeSlide, nextSlide) {
         activeSlide.classList.remove('number-card__slider-list-item_opacity1');
         activeSlide.classList.add('number-card__slider-list-item_opacity0');
         nextSlide.classList.remove('number-card__slider-list-item_opacity0');
         nextSlide.classList.add('number-card__slider-list-item_opacity1');
     }
 
-    switchToPreviousSlide(){
-        const activeSlide = this.numberCard.querySelector('.number-card__slider-list-item_opacity1');
-        const prevSlide = activeSlide.previousElementSibling;
-
-        activeSlide.classList.remove('number-card__slider-list-item_opacity1');
-        activeSlide.classList.add('number-card__slider-list-item_opacity0');
-        prevSlide.classList.remove('number-card__slider-list-item_opacity0');
-        prevSlide.classList.add('number-card__slider-list-item_opacity1');
+    switchSlideDot(activeDot, nextDot) {
+        activeDot.classList.remove('number-card__active-dot');
+        activeDot.classList.add('number-card__unactive-dot');
+        nextDot.classList.remove('number-card__unactive-dot');
+        nextDot.classList.add('number-card__active-dot');
     }
 }
 
