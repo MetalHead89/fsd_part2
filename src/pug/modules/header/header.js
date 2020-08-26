c4c29cee504f5c3122654f8308d8d2209f146fef
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-08-26 21:24:06 +0700

    fixed separator style

--- src/pug/modules/header/header.js
+++ src/pug/modules/header/header.js
@@ -4,6 +4,7 @@ let entryCheck = false;
 const entryBtn = document.querySelector('.header__button-entry-container');
 const registrationBtn = document.querySelector('.header__button-registration-container');
 const accountName = document.querySelector('.header__account-name');
+const separator = document.querySelector('.header__separator-item');
 
 entryBtn.onclick = signIn;
 accountName.onclick = signIn;
@@ -13,12 +14,14 @@ function signIn() {
         entryBtn.removeAttribute('style');
         registrationBtn.removeAttribute('style');
         accountName.removeAttribute('style');
+        separator.removeAttribute('style');
         entryCheck = false;
     } else {
         entryBtn.style.display = 'none';
         registrationBtn.style.display = 'none';
         accountName.style.display = 'flex'
         accountName.style.paddingRight = 0;
+        separator.style.display = 'flex';
         entryCheck = true;
     }
 }
