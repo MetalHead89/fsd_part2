f5ffce8bfa7ef6a6b29a756c0b0b3b270d2785e8
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-09-06 22:05:32 +0700

    Adjusted registration/sign in page responsiveness

--- src/js/registration_signin_page.js
+++ src/js/registration_signin_page.js
@@ -1,10 +1,15 @@
+'use strict'
+
 const header = document.querySelector('.header-container');
 const pageBodyBackground = document.querySelector('.registration-signin-page__body');
+const body = document.querySelector('body');
 
 window.onload = function() {
-    console.dir(header);
-    console.dir(header.offsetHeight);
-    console.dir(header.clientHeight);
-    console.dir(header.scrollHeight);
-    pageBodyBackground.style.height = `${parseInt(pageBodyBackground.offsetHeight) - parseInt(header.offsetHeight)}px`
+    const visiblePageHeight = document.documentElement.clientHeight;
+    if (document.querySelector('.entry-card') && visiblePageHeight >= 450 ||
+        document.querySelector('.registrationCard') && visiblePageHeight > 800) {
+            pageBodyBackground.style.height = `${document.documentElement.clientHeight - header.offsetHeight}px`;
+    } else if (document.querySelector('.entry-card') && visiblePageHeight < 450) {
+        pageBodyBackground.style.height = '450px';
+    }
 }
