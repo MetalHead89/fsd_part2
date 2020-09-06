c6d24ed389186928fb5070ab2fdb4d5cb2813167
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-09-06 21:04:44 +0700

    refactoring registration page

--- src/js/registration_page.js
+++
@@ -1,7 +0,0 @@
-const header = document.querySelector('.header-container');
-const pageBodyBackground = document.querySelector('.registration-page__body');
-console.dir(header);
-console.dir(header.offsetHeight);
-console.dir(header.clientHeight);
-console.dir(header.scrollHeight);
-pageBodyBackground.style.height = `${parseInt(pageBodyBackground.offsetHeight) - parseInt(header.offsetHeight)}px`
