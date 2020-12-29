f04cc347d9687cd1f0a0d40e2fecf15257735aeb
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2020-12-29 22:21:39 +0700

    Fixed eslint warnings in header.js

--- src/pug/modules/header/header.js
+++ src/pug/modules/header/header.js
@@ -1,14 +1,12 @@
-'use strict'
-
 const burger = document.querySelector('.header__burger');
 const hideMenu = document.querySelector('.navbar_vertical');
 const hideMenuItems = hideMenu.querySelectorAll('.navbar__item_vertical');
 
-burger.onclick = function() {
-    if (hideMenu) {
-        hideMenu.classList.toggle('navbar_vertical-opened');
-        for (let item of hideMenuItems) {
-            item.classList.toggle('navbar__item_vertical-opened');
-        }
+burger.onclick = () => {
+  if (hideMenu) {
+    hideMenu.classList.toggle('navbar_vertical-opened');
+    for (let itemIndex = 0; itemIndex < hideMenuItems.length; itemIndex += 1) {
+      hideMenuItems[itemIndex].classList.toggle('navbar__item_vertical-opened');
     }
-}
+  }
+};
