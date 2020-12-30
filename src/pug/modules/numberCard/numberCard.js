366de816e44888f86e20bd4c962099f27c7cedfd
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2020-12-30 08:28:38 +0700

    Fixed eslint warnings in numberCard.js.js

--- src/pug/modules/numberCard/numberCard.js
+++ src/pug/modules/numberCard/numberCard.js
@@ -1,71 +1,72 @@
-'use strict'
-
 class NumberCard {
-    constructor(numberCard) {
-        this.numberCard = numberCard;
+  constructor(numberCard) {
+    this.numberCard = numberCard;
 
-        this.buttonPrev = numberCard.querySelector('.number-card__prevButton');
-        this.buttonNext = numberCard.querySelector('.number-card__nextButton');
-        this.sliderDots = Array.from(numberCard.querySelectorAll('.number-card__dot'));
-        this.slides = numberCard.querySelectorAll('.number-card__slider-list-item');
-        // this.dotsContainer = numberCard.querySelector('.number-card__slider-dots');
+    this.init();
+  }
 
-        this.buttonNext.onclick = () =>  this.switchToNextSlide();
-        this.buttonPrev.onclick = () =>  this.switchToPreviousSlide();
-        for (let dot of this.sliderDots) {
-            dot.onclick = () => this.clickToDot(dot);
-        }
+  init() {
+    this.buttonPrev = this.numberCard.querySelector('.number-card__prevButton');
+    this.buttonNext = this.numberCard.querySelector('.number-card__nextButton');
+    this.sliderDots = Array.from(this.numberCard.querySelectorAll('.number-card__dot'));
+    this.slides = this.numberCard.querySelectorAll('.number-card__slider-list-item');
+
+    this.buttonNext.onclick = () => this.switchToNextSlide();
+    this.buttonPrev.onclick = () => this.switchToPreviousSlide();
+    for (let dot = 0; dot < this.sliderDots.length; dot += 1) {
+      this.sliderDots[dot].onclick = () => this.clickToDot(this.sliderDots[dot]);
     }
+  }
 
-    switchToNextSlide() {
-        const activeSlide = this.numberCard.querySelector('.number-card__slider-list-item_opacity1');
-        const nextSlide = activeSlide.nextElementSibling;
-        const activeDot = this.numberCard.querySelector('.number-card__active-dot');
-        const nextDot = activeDot.nextElementSibling;
+  switchToNextSlide() {
+    const activeSlide = this.numberCard.querySelector('.number-card__slider-list-item_opacity1');
+    const nextSlide = activeSlide.nextElementSibling;
+    const activeDot = this.numberCard.querySelector('.number-card__active-dot');
+    const nextDot = activeDot.nextElementSibling;
 
-        if (nextSlide) {
-            this.switchSlide(activeSlide, nextSlide);
-            this.switchSliderDot(activeDot, nextDot);
-        }
+    if (nextSlide) {
+      NumberCard.switchSlide(activeSlide, nextSlide);
+      NumberCard.switchSliderDot(activeDot, nextDot);
     }
+  }
 
-    switchToPreviousSlide(){
-        const activeSlide = this.numberCard.querySelector('.number-card__slider-list-item_opacity1');
-        const prevSlide = activeSlide.previousElementSibling;
-        const activeDot = this.numberCard.querySelector('.number-card__active-dot');
-        const prevDot = activeDot.previousElementSibling;
+  switchToPreviousSlide() {
+    const activeSlide = this.numberCard.querySelector('.number-card__slider-list-item_opacity1');
+    const prevSlide = activeSlide.previousElementSibling;
+    const activeDot = this.numberCard.querySelector('.number-card__active-dot');
+    const prevDot = activeDot.previousElementSibling;
 
-        if (prevSlide) {
-            this.switchSlide(activeSlide, prevSlide);
-            this.switchSliderDot(activeDot, prevDot);
-        }
+    if (prevSlide) {
+      NumberCard.switchSlide(activeSlide, prevSlide);
+      NumberCard.switchSliderDot(activeDot, prevDot);
     }
+  }
 
-    switchSlide(activeSlide, nextSlide) {
-        activeSlide.classList.remove('number-card__slider-list-item_opacity1');
-        activeSlide.classList.add('number-card__slider-list-item_opacity0');
-        nextSlide.classList.remove('number-card__slider-list-item_opacity0');
-        nextSlide.classList.add('number-card__slider-list-item_opacity1');
-    }
+  static switchSlide(activeSlide, nextSlide) {
+    activeSlide.classList.remove('number-card__slider-list-item_opacity1');
+    activeSlide.classList.add('number-card__slider-list-item_opacity0');
+    nextSlide.classList.remove('number-card__slider-list-item_opacity0');
+    nextSlide.classList.add('number-card__slider-list-item_opacity1');
+  }
 
-    clickToDot(dot) {
-        const activeDot = this.sliderDots.indexOf(this.numberCard.querySelector('.number-card__active-dot'));
-        const nextDot = this.sliderDots.indexOf(dot);
-        
-        this.switchSlide(this.slides[activeDot], this.slides[nextDot]);
-        this.switchSliderDot(this.sliderDots[activeDot], this.sliderDots[nextDot]);
-    }
+  clickToDot(dot) {
+    const activeDot = this.sliderDots.indexOf(this.numberCard.querySelector('.number-card__active-dot'));
+    const nextDot = this.sliderDots.indexOf(dot);
 
-    switchSliderDot(activeDot, nextDot) {
-        activeDot.classList.remove('number-card__active-dot');
-        activeDot.classList.add('number-card__unactive-dot');
-        nextDot.classList.remove('number-card__unactive-dot');
-        nextDot.classList.add('number-card__active-dot');
-    }
+    NumberCard.switchSlide(this.slides[activeDot], this.slides[nextDot]);
+    NumberCard.switchSliderDot(this.sliderDots[activeDot], this.sliderDots[nextDot]);
+  }
+
+  static switchSliderDot(activeDot, nextDot) {
+    activeDot.classList.remove('number-card__active-dot');
+    activeDot.classList.add('number-card__unactive-dot');
+    nextDot.classList.remove('number-card__unactive-dot');
+    nextDot.classList.add('number-card__active-dot');
+  }
 }
 
 const numbersCards = document.querySelectorAll('.number-card');
 
-for (let card of numbersCards) {
-    let numberCard = new NumberCard(card);
+for (let card = 0; card < numbersCards.length; card += 1) {
+  card = new NumberCard(numbersCards[card]);
 }
