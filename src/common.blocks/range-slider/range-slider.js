8da9ee33ab221139fc18556655bf4d63ab454168
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-01-21 15:21:33 +0700

    Changed logic of range-slider block

--- src/common.blocks/range-slider/range-slider.js
+++ src/common.blocks/range-slider/range-slider.js
@@ -1,46 +1,78 @@
-const leftInput = document.querySelector('.range-slider__input_left');
-const rightInput = document.querySelector('.range-slider__input_right');
-const leftThumb = document.querySelector('.range-slider__thumb_left');
-const rightThumb = document.querySelector('.range-slider__thumb_right');
-const range = document.querySelector('.range-slider__range');
+class Slider {
+  constructor(slider) {
+    this.slider = slider;
 
-const rangeLable = document.querySelector('.range-slider__range-label');
+    this.init();
+  }
 
-function setLeftValue() {
-  const that = leftInput;
-  const min = parseInt(that.min, 10);
-  const max = parseInt(that.max, 10);
+  init() {
+    this.leftInput = this.slider.querySelector('.range-slider__input_left');
+    this.rightInput = this.slider.querySelector('.range-slider__input_right');
+    this.leftThumb = this.slider.querySelector('.range-slider__thumb_left');
+    this.rightThumb = this.slider.querySelector('.range-slider__thumb_right');
+    this.range = this.slider.querySelector('.range-slider__range');
+    this.rangeLable = this.slider.querySelector('.range-slider__range-label');
 
-  that.value = Math.min(parseInt(that.value, 10), parseInt(rightInput.value, 10));
-  const percent = ((that.value - min) / (max - min)) * 100;
-  leftThumb.style.left = `${percent}%`;
-  range.style.left = `${percent}%`;
+    this.setLeftValue();
+    this.setRightValue();
 
-  const rangeNumbers = rangeLable.innerText.split('-');
-  rangeLable.innerText = `${parseInt(that.value, 10).toLocaleString('ru-RU')}${String.fromCharCode(8381)} - ${rangeNumbers[1]}`;
+    this.leftInput.addEventListener('input', this.setLeftValue.bind(this));
+    this.rightInput.addEventListener('input', this.setRightValue.bind(this));
+    this.leftInput.onmouseover = () => {
+      this.leftThumb.classList.add('range-slider__thumb_left_hover');
+    };
+    this.rightInput.onmouseover = () => {
+      this.rightThumb.classList.add('range-slider__thumb_right_hover');
+    };
+    this.leftInput.onmouseout = () => {
+      this.leftThumb.classList.remove('range-slider__thumb_left_hover');
+    };
+    this.rightInput.onmouseout = () => {
+      this.rightThumb.classList.remove('range-slider__thumb_right_hover');
+    };
+  }
+
+  setLeftValue() {
+    const that = this.leftInput;
+    const min = parseInt(that.min, 10);
+    const max = parseInt(that.max, 10);
+
+    that.value = Math.min(
+      parseInt(that.value, 10),
+      parseInt(this.rightInput.value, 10),
+    );
+    const percent = ((that.value - min) / (max - min)) * 100;
+    this.leftThumb.style.left = `${percent}%`;
+    this.range.style.left = `${percent}%`;
+
+    const rangeNumbers = this.rangeLable.innerText.split('-');
+    this.rangeLable.innerText = `${parseInt(that.value, 10).toLocaleString(
+      'ru-RU',
+    )}${String.fromCharCode(8381)} - ${rangeNumbers[1]}`;
+  }
+
+  setRightValue() {
+    const that = this.rightInput;
+    const min = parseInt(that.min, 10);
+    const max = parseInt(that.max, 10);
+
+    that.value = Math.max(
+      parseInt(that.value, 10),
+      parseInt(this.leftInput.value, 10) + 1,
+    );
+    const percent = ((that.value - min) / (max - min)) * 100;
+    this.rightThumb.style.right = `${100 - percent}%`;
+    this.range.style.right = `${100 - percent}%`;
+
+    const rangeNumbers = this.rangeLable.innerText.split('-');
+    this.rangeLable.innerText = `${rangeNumbers[0]} - ${parseInt(
+      that.value,
+      10,
+    ).toLocaleString('ru-RU')}${String.fromCharCode(8381)}`;
+  }
 }
 
-setLeftValue();
-
-function setRightValue() {
-  const that = rightInput;
-  const min = parseInt(that.min, 10);
-  const max = parseInt(that.max, 10);
-
-  that.value = Math.max(parseInt(that.value, 10), parseInt(leftInput.value, 10) + 1);
-  const percent = ((that.value - min) / (max - min)) * 100;
-  rightThumb.style.right = `${100 - percent}%`;
-  range.style.right = `${100 - percent}%`;
-
-  const rangeNumbers = rangeLable.innerText.split('-');
-  rangeLable.innerText = `${rangeNumbers[0]} - ${parseInt(that.value, 10).toLocaleString('ru-RU')}${String.fromCharCode(8381)}`;
+const sliders = document.querySelectorAll('.range-slider');
+for (let slider = 0; slider < sliders.length; slider += 1) {
+  const newSlider = new Slider(sliders[slider]);
 }
-
-setRightValue();
-
-leftInput.addEventListener('input', setLeftValue);
-rightInput.addEventListener('input', setRightValue);
-leftInput.onmouseover = () => { leftThumb.classList.add('range-slider__thumb_left_hover'); };
-rightInput.onmouseover = () => { rightThumb.classList.add('range-slider__thumb_right_hover'); };
-leftInput.onmouseout = () => { leftThumb.classList.remove('range-slider__thumb_left_hover'); };
-rightInput.onmouseout = () => { rightThumb.classList.remove('range-slider__thumb_right_hover'); };
