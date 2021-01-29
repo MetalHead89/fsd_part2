06eb59a25b5163656e20eb5a28e6bd6688dd42a9
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-01-29 22:50:07 +0700

    Moved common styles to ui-kit.scss and layout-for-pages.scss files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // Styles
 import '@/pages/ui-kit/ui-kit.scss';
-import '@/scss/fonts.scss';
-import '@/scss/style.scss';
+import '@/pages/layout-for-pages/layout-for-pages.scss';
 import '@/pages/landing-page/landing-page.scss';
 import '@/pages/search-room/search-room.scss';
 import '@/pages/room-details/room-details.scss';
