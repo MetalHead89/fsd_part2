b9deb351dd558d176123808c7457a47140755b7b
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-04-14 16:58:35 +0700

    Исправено отсутствие реакции после клика по бургер-меню

--- src/common.blocks/header/header.js
+++ src/common.blocks/header/header.js
@@ -8,17 +8,25 @@ class BurgerMenu {
   init() {
     const hideMenu = document.querySelector('.navbar__list_vertical');
     const hideMenuItems = hideMenu.querySelectorAll('.navbar__item_vertical');
+    hideMenu.classList.add('navbar__list_vertical-closed');
+    hideMenuItems.forEach((item) =>
+      item.classList.add('navbar__item_vertical-closed'),
+    );
 
     this.burger.onclick = () => {
       if (hideMenu) {
         hideMenu.classList.toggle('navbar__list_vertical-opened');
+        hideMenu.classList.toggle('navbar__list_vertical-closed');
         for (
           let itemIndex = 0;
           itemIndex < hideMenuItems.length;
           itemIndex += 1
         ) {
           hideMenuItems[itemIndex].classList.toggle(
-            'navbar__item_vertical-opened'
+            'navbar__item_vertical-opened',
+          );
+          hideMenuItems[itemIndex].classList.toggle(
+            'navbar__item_vertical-closed',
           );
         }
       }
