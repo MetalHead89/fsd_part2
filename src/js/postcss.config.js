1543ea904465e3cb2edc3c433550fdd20abf1946
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-05-03 20:44:58 +0700

    Установлен cssnano

--- src/js/postcss.config.js
+++ src/js/postcss.config.js
@@ -2,15 +2,15 @@ module.exports = {
   plugins: [
     require('autoprefixer'),
     require('css-mqpacker'),
-    // require('cssnano')({
-    //   preset: [
-    //     'default',
-    //     {
-    //       discardComments: {
-    //         removeAll: true,
-    //       },
-    //     },
-    //   ],
-    // }),
+    require('cssnano')({
+      preset: [
+        'default',
+        {
+          discardComments: {
+            removeAll: true,
+          },
+        },
+      ],
+    }),
   ],
 };
