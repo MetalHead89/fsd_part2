60ea686cb54253bc38bdb09e426d6dfe8d8ab3e1
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-06-03 08:48:19 +0700

    #13 number переименовано в room, удалены задублированные классы, изменены сокращения слов в названиях классов на полные версии

---
+++ src/common.blocks/room-card/room-card.js
@@ -0,0 +1,92 @@
+class RoomCard {
+  constructor(roomCard) {
+    this.roomCard = roomCard;
+
+    this.init();
+  }
+
+  init() {
+    this.buttonPrev = this.roomCard.querySelector(
+      '.room-card__arrow-button_prev'
+    );
+    this.buttonNext = this.roomCard.querySelector(
+      '.room-card__arrow-button_next'
+    );
+    this.sliderDots = Array.from(
+      this.roomCard.querySelectorAll('.room-card__dot')
+    );
+    this.slides = this.roomCard.querySelectorAll(
+      '.room-card__slider-list-item'
+    );
+
+    this.buttonNext.onclick = () => this.switchToNextSlide();
+    this.buttonPrev.onclick = () => this.switchToPreviousSlide();
+    for (let dot = 0; dot < this.sliderDots.length; dot += 1) {
+      this.sliderDots[dot].onclick = () => {
+        this.clickToDot(this.sliderDots[dot]);
+      };
+    }
+  }
+
+  switchToNextSlide() {
+    const activeSlide = this.roomCard.querySelector(
+      '.room-card__slider-list-item_transparent'
+    );
+    const nextSlide = activeSlide.nextElementSibling;
+    const activeDot = this.roomCard.querySelector('.room-card__dot_active');
+    const nextDot = activeDot.nextElementSibling;
+
+    if (nextSlide) {
+      RoomCard.switchSlide(activeSlide, nextSlide);
+      RoomCard.switchSliderDot(activeDot, nextDot);
+    }
+  }
+
+  switchToPreviousSlide() {
+    const activeSlide = this.roomCard.querySelector(
+      '.room-card__slider-list-item_transparent'
+    );
+    const prevSlide = activeSlide.previousElementSibling;
+    const activeDot = this.roomCard.querySelector('.room-card__dot_active');
+    const prevDot = activeDot.previousElementSibling;
+
+    if (prevSlide) {
+      RoomCard.switchSlide(activeSlide, prevSlide);
+      RoomCard.switchSliderDot(activeDot, prevDot);
+    }
+  }
+
+  static switchSlide(activeSlide, nextSlide) {
+    activeSlide.classList.remove('room-card__slider-list-item_transparent');
+    activeSlide.classList.add('room-card__slider-list-item_not-transparent');
+    nextSlide.classList.remove('room-card__slider-list-item_not-transparent');
+    nextSlide.classList.add('room-card__slider-list-item_transparent');
+  }
+
+  clickToDot(dot) {
+    const activeDot = this.sliderDots.indexOf(
+      this.roomCard.querySelector('.room-card__dot_active')
+    );
+    const nextDot = this.sliderDots.indexOf(dot);
+
+    RoomCard.switchSlide(this.slides[activeDot], this.slides[nextDot]);
+    RoomCard.switchSliderDot(
+      this.sliderDots[activeDot],
+      this.sliderDots[nextDot]
+    );
+  }
+
+  static switchSliderDot(activeDot, nextDot) {
+    activeDot.classList.remove('room-card__dot_active');
+    activeDot.classList.add('room-card__dot_inactive');
+    nextDot.classList.remove('room-card__dot_inactive');
+    nextDot.classList.add('room-card__dot_active');
+  }
+}
+
+const roomCards = document.querySelectorAll('.room-card');
+const cards = [];
+
+for (let cardIndex = 0; cardIndex < roomCards.length; cardIndex += 1) {
+  cards.push(new RoomCard(roomCards[cardIndex]));
+}
