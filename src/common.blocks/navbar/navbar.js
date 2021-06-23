69b402aeb7fd8d61a032a02ee0ac9758039061ac
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-06-23 12:48:06 +0700

    Удален лишний код из класса Navbar

--- src/common.blocks/navbar/navbar.js
+++ src/common.blocks/navbar/navbar.js
@@ -8,16 +8,14 @@ class Navbar {
 
   init() {
     this.isEntryCheck = false;
-    this.entryButtons = this.navbar.querySelectorAll(
+    this.entryButton = this.navbar.querySelector(
       '.js-navbar__button-entry-container',
     );
-    this.registrationButtons = this.navbar.querySelectorAll(
+    this.registrationButton = this.navbar.querySelector(
       '.js-navbar__button-registration-container',
     );
-    this.accountNames = this.navbar.querySelectorAll(
-      '.js-navbar__item_with-name',
-    );
-    this.separators = this.navbar.querySelectorAll(
+    this.accountName = this.navbar.querySelector('.js-navbar__item_with-name');
+    this.separator = this.navbar.querySelector(
       '.js-navbar__item_with-separator',
     );
     this.dropdownItems = this.navbar.querySelectorAll(
@@ -31,26 +29,20 @@ class Navbar {
   }
 
   addEventListeners() {
-    for (let button = 0; button < this.entryButtons.length; button += 1) {
-      this.entryButtons[button].addEventListener(
-        'click',
-        this.handleButtonEntryClick.bind(this),
-      );
-    }
+    this.entryButton.addEventListener(
+      'click',
+      this.handleButtonEntryClick.bind(this),
+    );
 
-    for (let name = 0; name < this.accountNames.length; name += 1) {
-      this.accountNames[name].addEventListener(
-        'click',
-        this.handleNavbarItemWithNameClick.bind(this),
-      );
-    }
+    this.accountName.addEventListener(
+      'click',
+      this.handleNavbarItemWithNameClick.bind(this),
+    );
 
-    for (let item = 0; item < this.dropdownItems.length; item += 1) {
-      this.dropdownItems[item].addEventListener(
-        'click',
-        this.handleDropdownTitleClick,
-      );
-    }
+    this.dropdownItems.forEach((item) => {
+      const modifyItem = item;
+      modifyItem.addEventListener('click', this.handleDropdownTitleClick);
+    });
   }
 
   handleButtonEntryClick() {
@@ -72,49 +64,17 @@ class Navbar {
 
   signIn() {
     if (this.isEntryCheck) {
-      for (let button = 0; button < this.entryButtons.length; button += 1) {
-        this.entryButtons[button].removeAttribute('style');
-      }
-      for (
-        let button = 0;
-        button < this.registrationButtons.length;
-        button += 1
-      ) {
-        this.registrationButtons[button].removeAttribute('style');
-      }
-      for (let name = 0; name < this.accountNames.length; name += 1) {
-        this.accountNames[name].removeAttribute('style');
-      }
-      for (
-        let separator = 0;
-        separator < this.separators.length;
-        separator += 1
-      ) {
-        this.separators[separator].removeAttribute('style');
-      }
+      this.entryButton.removeAttribute('style');
+      this.registrationButton.removeAttribute('style');
+      this.accountName.removeAttribute('style');
+      this.separator.removeAttribute('style');
       this.isEntryCheck = false;
     } else {
-      for (let button = 0; button < this.entryButtons.length; button += 1) {
-        this.entryButtons[button].style.display = 'none';
-      }
-      for (
-        let button = 0;
-        button < this.registrationButtons.length;
-        button += 1
-      ) {
-        this.registrationButtons[button].style.display = 'none';
-      }
-      for (let name = 0; name < this.accountNames.length; name += 1) {
-        this.accountNames[name].style.display = 'flex';
-        this.accountNames[name].style.paddingRight = 0;
-      }
-      for (
-        let separator = 0;
-        separator < this.separators.length;
-        separator += 1
-      ) {
-        this.separators[separator].style.display = 'flex';
-      }
+      this.entryButton.style.display = 'none';
+      this.registrationButton.style.display = 'none';
+      this.accountName.style.display = 'flex';
+      this.accountName.style.paddingRight = 0;
+      this.separator.style.display = 'flex';
       this.isEntryCheck = true;
     }
   }
