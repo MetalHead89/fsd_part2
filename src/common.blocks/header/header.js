6bdc783dbe9f2a83fe54056231f0c1a87750af9b
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-07-02 08:49:56 +0700

    #23 Исправлен блок header, больше он не управляет блоком navbar

--- src/common.blocks/header/header.js
+++ src/common.blocks/header/header.js
@@ -8,10 +8,7 @@ class Header {
 
   _init() {
     this._burger = this._header.querySelector('.js-header__burger');
-    this._hideMenu = this._header.querySelector('.js-navbar__list_vertical');
-    this._hideMenuItems = this._hideMenu.querySelectorAll(
-      '.js-navbar__item_vertical',
-    );
+    this._hideMenu = this._header.querySelector('.js-header__hidden-navbar');
 
     this._handleBurgerClick = this._handleBurgerClick.bind(this);
   }
@@ -22,14 +19,7 @@ class Header {
 
   _handleBurgerClick() {
     if (this._hideMenu) {
-      this._hideMenu.classList.toggle('navbar__list_opened');
-      for (
-        let itemIndex = 0;
-        itemIndex < this._hideMenuItems.length;
-        itemIndex += 1
-      ) {
-        this._hideMenuItems[itemIndex].classList.toggle('navbar__item_opened');
-      }
+      this._hideMenu.classList.toggle('header__hidden-navbar_opened');
     }
   }
 }
