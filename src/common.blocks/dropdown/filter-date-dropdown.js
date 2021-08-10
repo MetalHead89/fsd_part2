92a7e73b14ec4522cfd9b161ed0b3fe2f0fa1f21
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-08-10 20:13:55 +0700

    Удален компонент dropdown

--- src/common.blocks/dropdown/filter-date-dropdown.js
+++
@@ -1,9 +0,0 @@
-import Dropdown from './dropdown';
-
-class FilterDateDropdown extends Dropdown {
-  constructor(dropdown) {
-    super(dropdown, 'filterDate');
-  }
-}
-
-export default FilterDateDropdown;
