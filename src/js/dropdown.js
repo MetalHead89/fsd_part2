f5de67500424b8e26725c168933995685be1cff7
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-08-25 22:32:19 +0700

    Добавлены методы для определения клика вне дропдауна в классе Dropdown

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -9,19 +9,38 @@ class Dropdown {
     this._addEventListeners();
   }
 
+  static handleBodyClick(evt) {
+    if (Dropdown.clickIsOutsideDropdown(evt)) {
+      // Dropdown.closeAllDropdowns();
+    }
+  }
+
+  static clickIsOutsideDropdown(evt) {
+    return !Dropdown.dropdownsOnThePage.some(
+      (dropdown) => evt.path.indexOf(dropdown._dropdown) >= 0
+    );
+  }
+
+  // static closeAllDropdowns() {
+  //   Dropdown.dropdownsOnThePage.forEach();
+  // }
+
   _init() {
     if (Dropdown.dropdownsOnThePage === undefined) {
       Dropdown.dropdownsOnThePage = [];
     }
-    Dropdown.dropdownsOnThePage.push(this._dropdown);
+    Dropdown.dropdownsOnThePage.push(this);
 
     this._opened = false;
+
+    this._handleBodyClick = Dropdown.handleBodyClick.bind(this);
   }
 
   _addEventListeners() {
     this._dropdownHeader.addClickToHeaderListener(
       this._toggleDropdownState.bind(this)
     );
+    document.body.addEventListener('mouseup', this._handleBodyClick);
   }
 
   _toggleDropdownState() {
