65e1ed70a3e49afef7cbfbcde2ccd96ad28351a4
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-08-25 10:30:07 +0700

    Изменены стили компонента dropdown-header, добавлено свойство isStraightBottom в миксин dropdown-header, выпремляющее нижние углы элемента

---
+++ src/common.blocks/comfort-dropdown_old/init.js
@@ -0,0 +1,20 @@
+// /* eslint-disable no-new */
+
+// import ComfortDropdown from './comfort-dropdown';
+// import CountingMenuStore from '../counting-menu/counting-menu-store';
+// import DropdownStore from '../dropdown/dropdown-store';
+
+// require('../dropdown/init');
+// require('../counting-menu/init');
+
+// const comfortDropdowns = document.querySelectorAll('.js-comfort-dropdown');
+
+// comfortDropdowns.forEach((comfortDropdown) => {
+//   const dropdown = DropdownStore.getDropdownByElement(
+//     comfortDropdown.querySelector('.js-dropdown')
+//   );
+//   const countingMenu = CountingMenuStore.getMenuByElement(
+//     comfortDropdown.querySelector('.js-counting-menu')
+//   );
+//   new ComfortDropdown(comfortDropdown, dropdown, countingMenu);
+// });
