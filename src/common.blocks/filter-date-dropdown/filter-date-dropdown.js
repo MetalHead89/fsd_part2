3d7d1faec3a3c505231aaf2859f55fad576edfad
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-08-26 12:41:02 +0700

    Переделан компонент filter-date-dropdown

--- src/common.blocks/filter-date-dropdown/filter-date-dropdown.js
+++ src/common.blocks/filter-date-dropdown/filter-date-dropdown.js
@@ -1,21 +1,44 @@
 // /* eslint-disable comma-dangle */
 
-// class FilterDateDropdown {
-//   constructor(filterDateDropdown, dropdown) {
-//     this._filterDateDropdown = filterDateDropdown;
-//     this._dropdown = dropdown;
+import Dropdown from '../../js/dropdown';
 
-//     this._dropdown.subscribeToClickOnClearButton(this._clear.bind(this));
-//   }
+class FilterDateDropdown extends Dropdown {
+  // constructor(dropdown, dropdownHeader, dropMenu) {
+  //   super(dropdown, dropdownHeader, dropMenu);
+  //   this._init();
+  // }
+  // _init() {
+  //   super._init();
+  //   this._countingMenu.addClickToButtonListener(
+  //     this._changeDropdownHeaderText.bind(this)
+  //   );
+  //   this.WORDS = {
+  //     bedrooms: ['спальня', 'спальни', 'спален'],
+  //     beds: ['кровать', 'кровати', 'кроватей'],
+  //     bathroom: ['ванная комната', 'ванные комнаты', 'ванных комнат'],
+  //   };
+  // }
+  // _changeDropdownHeaderText() {
+  //   const counters = this._countingMenu.getCounters();
+  //   const bedrooms = counters[0];
+  //   const beds = counters[1];
+  //   const bathroom = counters[2];
+  //   let headerText = null;
+  //   if (bedrooms > 0) {
+  //     headerText = `${bedrooms} ${this._getWord(bedrooms, 'bedrooms')}`;
+  //   }
+  //   if (beds > 0) {
+  //     headerText = headerText
+  //       ? `${headerText}, ${beds} ${this._getWord(beds, 'beds')}`
+  //       : `${beds} ${this._getWord(beds, 'beds')}`;
+  //   }
+  //   if (bathroom > 0) {
+  //     headerText = headerText
+  //       ? `${headerText}, ${bathroom} ${this._getWord(bathroom, 'bathroom')}`
+  //       : `${bathroom} ${this._getWord(bathroom, 'bathroom')}`;
+  //   }
+  //   this._changeHeader(headerText);
+  // }
+}
 
-//   _changeDropdownHeaderText() {
-//     const headerText = 'eeeeee';
-//     this._dropdown.setHeaderText(headerText);
-//   }
-
-//   _clear() {
-//     this._changeDropdownHeaderText();
-//   }
-// }
-
-// export default FilterDateDropdown;
+export default FilterDateDropdown;
--- src/common.blocks/filter-date-dropdown/init.js
+++ src/common.blocks/filter-date-dropdown/init.js
@@ -1,21 +1,21 @@
-// /* eslint-disable no-new */
+/* eslint-disable no-new */
 
-// import FilterDateDropdown from './filter-date-dropdown';
-// import DropdownStore from '../dropdown/dropdown-store';
+import FilterDateDropdown from './filter-date-dropdown';
+import DropdownHeaderStore from '../dropdown-header/dropdown-header-store';
+import DropMenuStore from '../drop-menu/drop-menu-store';
 
-// require('../dropdown/init');
+require('../dropdown-header/init');
+require('../drop-menu/init');
 
-// const filterDateDropdowns = document.querySelectorAll('.js-comfort-dropdown');
+const dropdowns = document.querySelectorAll('.js-filter-date-dropdown');
 
-// filterDateDropdowns.forEach((filterDateDropdown) => {
-//   const dropdown = DropdownStore.getDropdownByElement(
-//     filterDateDropdown.querySelector('.js-dropdown')
-//   );
-//   new FilterDateDropdown(filterDateDropdown, dropdown);
-// });
+dropdowns.forEach((dropdown) => {
+  const header = DropdownHeaderStore.getHeaderByElement(
+    dropdown.querySelector('.js-dropdown-header')
+  );
+  const dropMenu = DropMenuStore.getDropMenuByElement(
+    dropdown.querySelector('.js-drop-menu')
+  );
 
-// // import FilterDateDropdown from './filter-date-dropdown';
-
-// // const dropdowns = document.querySelectorAll('.js-filter-date-dropdown');
-
-// // dropdowns.forEach((item) => new FilterDateDropdown(item));
+  new FilterDateDropdown(dropdown, header, dropMenu);
+});
