6fd1f970cf8e7d932d9e5774039f135c42d13af6
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-08-26 09:48:22 +0700

    Удален лишний код из файла инициализации компонента comfort-dropdown

--- src/common.blocks/comfort-dropdown/init.js
+++ src/common.blocks/comfort-dropdown/init.js
@@ -24,24 +24,3 @@ dropdowns.forEach((dropdown) => {
 
   new ComfortDropdown(dropdown, header, dropMenu, countingMenu);
 });
-
-// /* eslint-disable no-new */
-
-// import ComfortDropdown from './comfort-dropdown';
-// import CountingMenuStore from '../counting-menu/counting-menu-store';
-// import DropdownStore from '../dropdown/dropdown-store';
-
-// require('../dropdown/init');
-// require('../counting-menu/init');
-
-// const comfortDropdowns = document.querySelectorAll('.js-comfort-dropdown');
-
-// comfortDropdowns.forEach((comfortDropdown) => {
-//   const dropdown = DropdownStore.getDropdownByElement(
-//     comfortDropdown.querySelector('.js-dropdown')
-//   );
-//   const countingMenu = CountingMenuStore.getMenuByElement(
-//     comfortDropdown.querySelector('.js-counting-menu')
-//   );
-//   new ComfortDropdown(comfortDropdown, dropdown, countingMenu);
-// });
