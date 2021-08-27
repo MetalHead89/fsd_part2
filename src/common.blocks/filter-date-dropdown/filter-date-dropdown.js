d9752127b32e141728feeb64255252cb4bc3550b
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-08-27 07:26:37 +0700

    Удален старый код из filter-date-dropdown

--- src/common.blocks/filter-date-dropdown/filter-date-dropdown.js
+++ src/common.blocks/filter-date-dropdown/filter-date-dropdown.js
@@ -30,43 +30,6 @@ class FilterDateDropdown extends Dropdown {
 
     super._changeHeader(headerText);
   }
-
-  // constructor(dropdown, dropdownHeader, dropMenu) {
-  //   super(dropdown, dropdownHeader, dropMenu);
-  //   this._init();
-  // }
-  // _init() {
-  //   super._init();
-  //   this._countingMenu.addClickToButtonListener(
-  //     this._changeDropdownHeaderText.bind(this)
-  //   );
-  //   this.WORDS = {
-  //     bedrooms: ['спальня', 'спальни', 'спален'],
-  //     beds: ['кровать', 'кровати', 'кроватей'],
-  //     bathroom: ['ванная комната', 'ванные комнаты', 'ванных комнат'],
-  //   };
-  // }
-  // _changeDropdownHeaderText() {
-  //   const counters = this._countingMenu.getCounters();
-  //   const bedrooms = counters[0];
-  //   const beds = counters[1];
-  //   const bathroom = counters[2];
-  //   let headerText = null;
-  //   if (bedrooms > 0) {
-  //     headerText = `${bedrooms} ${this._getWord(bedrooms, 'bedrooms')}`;
-  //   }
-  //   if (beds > 0) {
-  //     headerText = headerText
-  //       ? `${headerText}, ${beds} ${this._getWord(beds, 'beds')}`
-  //       : `${beds} ${this._getWord(beds, 'beds')}`;
-  //   }
-  //   if (bathroom > 0) {
-  //     headerText = headerText
-  //       ? `${headerText}, ${bathroom} ${this._getWord(bathroom, 'bathroom')}`
-  //       : `${bathroom} ${this._getWord(bathroom, 'bathroom')}`;
-  //   }
-  //   this._changeHeader(headerText);
-  // }
 }
 
 export default FilterDateDropdown;
