30e3e64f4388b839366cc90d1da9e55bf96f6305
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-08-30 09:16:39 +0700

    Удален старый компонент dropdown

--- src/common.blocks/dropdown/init.js
+++
@@ -1,6 +0,0 @@
-// import Dropdown from './dropdown';
-// import DropdownStore from './dropdown-store';
-
-// const dropdowns = document.querySelectorAll('.js-dropdown');
-
-// dropdowns.forEach((item) => DropdownStore.addDropdown(new Dropdown(item)));
