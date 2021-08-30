01bffede1219197ffbe6b7b5dcb21b982dd524f4
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-08-30 08:38:03 +0700

    Удален лишний код из counting-menu

--- src/common.blocks/counting-menu/counting-menu.js
+++ src/common.blocks/counting-menu/counting-menu.js
@@ -35,10 +35,6 @@ class CountingMenu {
     return this._countingMenu;
   }
 
-  // subscribe(func) {
-  //   this._observers.push(func);
-  // }
-
   addClickToButtonListener(callback) {
     this._clickToButtonListeners.push(callback);
   }
@@ -53,32 +49,6 @@ class CountingMenu {
     return counters.map((counter) => parseInt(counter.innerText, 10));
   }
 
-  // addClickButtonObserver(func) {
-  //   this.observer = func;
-  // }
-
-  // _changeDropdownHeaderText(items, defaultHeaderText) {
-  //   let headerText = '';
-
-  //   Object.keys(items).forEach((key) => {
-  //     if (items[key] > 0) {
-  //       if (headerText !== '') {
-  //         headerText += ', ';
-  //       }
-  //       headerText += `${items[key]} ${QuantityDropdown.dropdownWordGenerator(
-  //         this._headerWords[key],
-  //         items[key]
-  //       )}`;
-  //     }
-  //   });
-
-  //   if (headerText === '') {
-  //     headerText = defaultHeaderText;
-  //   }
-
-  //   this._setHeaderText(headerText);
-  // }
-
   _increaseQuantitySum() {
     this._quantitySum += 1;
   }
@@ -86,27 +56,6 @@ class CountingMenu {
   _decreaseQuantitySum() {
     this._quantitySum -= 1;
   }
-
-  // static numberIsTwoThreeOrFour(number10, number100) {
-  //   return (
-  //     number10 >= 2 && number10 <= 4 && !(number100 >= 12 && number100 <= 14)
-  //   );
-  // }
-
-  // static dropdownWordGenerator(words, number) {
-  //   const number10 = number % 10;
-  //   const number100 = number % 100;
-
-  //   if (number10 === 1 && number100 !== 11) {
-  //     return words[0];
-  //   }
-
-  //   if (QuantityDropdown.numberIsTwoThreeOrFour(number10, number100)) {
-  //     return words[1];
-  //   }
-
-  //   return words[2];
-  // }
 
   _handleQuantityButtonClick(event) {
     let quantityElement = null;
@@ -125,10 +74,6 @@ class CountingMenu {
     if (this._clickToButtonListeners.length > 0) {
       this._clickToButtonListeners.forEach((listener) => listener());
     }
-    // if (this._type === 'guests-dropdown') {
-    //   this._onOffClearButton();
-    // }
-    // this._changeDropdownHeaderText();
   }
 
   _onOffQuantityButton() {
@@ -138,14 +83,6 @@ class CountingMenu {
       this.previousElementSibling.disabled = true;
     }
   }
-
-  // _onOffClearButton() {
-  //   if (this._quantitySum > 0) {
-  //     this._clearButtonShow();
-  //   } else {
-  //     this._clearButtonHide();
-  //   }
-  // }
 }
 
 export default CountingMenu;
